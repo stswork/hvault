742414b0104705845007094e31b4b7f64043b597
Shubho Pramanik <shubho.pramanik@gmail.com>
2014-04-20 00:51:27 +0530

    UI -Added

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,18 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+    public static Result login() {
+        return ok(login.render("Login"));
+    }
+    public static Result dashboard() {
+        return ok(dashboard.render("Dashboard"));
+    }
 
+    public static Result profile() {
+        return ok(profile.render("Profile"));
+    }
+
+    public static Result form() {
+        return ok(form.render("Form"));
+    }
 }
